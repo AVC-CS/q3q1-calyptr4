e6b6f814d7f61aa6e23faea413759520c6a5062a
calyptr4 <alessandrabaires@gmail.com>
2026-03-14 19:53:09 -0700

    q3q1

--- main.cpp
+++ main.cpp
@@ -5,21 +5,20 @@ using namespace std;
 
 int main()
 {
-    double a, b, c;
-   double x1, x2;
+    double a, b, c, x1, x2;
 
    cout << "type 3 numbers. \n";
    cin >> a >> b >> c;
 
-   x1 = (-b + sqrt (pow(b,2) - 4 * a * c )) / (2 * a);
-   x2 = (-b - sqrt (pow(b,2) - 4 * a * c )) / (2 * a);
+   x1 = (-b + sqrt (abs(pow(b,2) - 4 * a * c))) / (2 * a);
+   x2 = (-b - sqrt (abs(pow(b,2) - 4 * a * c))) / (2 * a);
    
    cout << setprecision(2) << fixed;
    
    if (x1 > x2) {
-    cout << x1 << ", " << x2 << endl;
+    cout << x1 << " " << x2 << endl;
    } else {
-    cout << x2 << ", " << x1 << endl;
+    cout << x2 << " " << x1 << endl;
    }
 
     return 0;
