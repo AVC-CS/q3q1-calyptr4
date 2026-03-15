2a545e9c9a63ce3310d04b37543458350239d8ff
calyptr4 <alessandrabaires@gmail.com>
2026-03-14 19:08:04 -0700

    q3q1

--- main.cpp
+++ main.cpp
@@ -6,17 +6,21 @@ using namespace std;
 int main()
 {
     double a, b, c;
-    // TODO
+   double x1, x2;
 
-    // TODO
-    // TODO
-    // TODO
+   cout << "type 3 numbers. \n";
+   cin >> a >> b >> c;
 
-    // TODO
-    // TODO
-
-    cout << setprecision(2) << fixed;
-    // TODO
+   x1 = (-b + sqrt (pow(b,2) - 4 * a * c )) / (2 * a);
+   x2 = (-b - sqrt (pow(b,2) - 4 * a * c )) / (2 * a);
+   
+   cout << setprecision(2) << fixed;
+   
+   if (x1 > x2) {
+    cout << x1 << ", " << x2 << endl;
+   } else {
+    cout << x2 << ", " << x1 << endl;
+   }
 
     return 0;
 }
